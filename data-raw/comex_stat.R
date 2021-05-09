b957b13cdd78486219766f912a9a417203556b2d
fernandobastosneto <fernando.bastos.neto@gmail.com>
2021-05-09 00:06:47 -0300

    colocando o maggritr logo

--- data-raw/comex_stat.R
+++ data-raw/comex_stat.R
@@ -1,3 +1,5 @@
+library(magrittr)
+
 ultimo_ano <- barao::comerciobr_get_ulimoano()
 
 url <- paste0("http://api.comexstat.mdic.gov.br/general?filter=%7B%22yearStart%22:%221997%22,%22yearEnd%22:%22", ultimo_ano,"%22,%22typeForm%22:1,%22typeOrder%22:2,%22filterList%22:%5B%5D,%22filterArray%22:%5B%5D,%22rangeFilter%22:%5B%5D,%22detailDatabase%22:%5B%7B%22id%22:%22noSh2pt%22,%22text%22:%22Cap%C3%ADtulo%20(SH2)%22,%22parentId%22:%22coSh2%22,%22parent%22:%22Codigo%20SH2%22%7D%5D,%22monthDetail%22:true,%22metricFOB%22:true,%22metricKG%22:false,%22metricStatistic%22:false,%22monthStart%22:%2201%22,%22monthEnd%22:%2212%22,%22formQueue%22:%22general%22,%22langDefault%22:%22pt%22,%22monthStartName%22:%22Janeiro%22,%22monthEndName%22:%22Dezembro%22%7D")
